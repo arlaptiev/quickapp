b810f68562de13b009f4b222c03a07786b4355c7
Artem Laptiev <arlaptiev@gmail.com>
2022-12-06 13:32:13 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './assets/css/main.css';
+import './assets/css/fonts.css';
+import './assets/css/features.css';
+import App from './pages/App';
+import reportWebVitals from './utils/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div class="container m-auto">
+      <p class="text-center text-6xl font-bold mt-44"> Quick App </p>
+    </div>
+  );
+}
+
+export default App;
